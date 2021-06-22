5d8fa82e2d1533b3ca4fcb7bdbfd5545014c0d7d
angelp495 <angeljpa95@gmail.com>
2021-06-22 12:45:44 -0500

    inner layout and body details

--- main.js
+++ main.js
@@ -2,14 +2,50 @@ const d = document,
   $stats = d.getElementsByClassName("stat"),
   $buyerCards = d.getElementsByClassName("buyer-card");
 
-d.addEventListener("DOMContentLoaded", (e) => {
-  for (let i = 0; i < $stats.length; i++) {
-    const stat = $stats[i];
-    stat.style.marginLeft = `${i * 50}px`;
-  }
+window.addEventListener("resize", (e) => {
+  if (window.innerWidth >= 900) {
+    for (let i = 0; i < $stats.length; i++) {
+      const stat = $stats[i];
+      stat.style.marginLeft = `${i * 50}px`;
+    }
 
-  for (let i = 0; i < $buyerCards.length; i++) {
-    const card = $buyerCards[i];
-    card.style.marginTop = `${i * 20}px`;
+    for (let i = 0; i < $buyerCards.length; i++) {
+      const card = $buyerCards[i];
+      card.style.marginTop = `${i * 20}px`;
+    }
+  } else {
+    for (let i = 0; i < $stats.length; i++) {
+      const stat = $stats[i];
+      stat.style.marginLeft = `0px`;
+    }
+
+    for (let i = 0; i < $buyerCards.length; i++) {
+      const card = $buyerCards[i];
+      card.style.marginTop = `0px`;
+    }
+  }
+});
+
+d.addEventListener("DOMContentLoaded", (e) => {
+  if (window.innerWidth >= 900) {
+    for (let i = 0; i < $stats.length; i++) {
+      const stat = $stats[i];
+      stat.style.marginLeft = `${i * 50}px`;
+    }
+
+    for (let i = 0; i < $buyerCards.length; i++) {
+      const card = $buyerCards[i];
+      card.style.marginTop = `${i * 20}px`;
+    }
+  } else {
+    for (let i = 0; i < $stats.length; i++) {
+      const stat = $stats[i];
+      stat.style.marginLeft = `0px`;
+    }
+
+    for (let i = 0; i < $buyerCards.length; i++) {
+      const card = $buyerCards[i];
+      card.style.marginTop = `0px`;
+    }
   }
 });
